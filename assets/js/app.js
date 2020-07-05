cc46c13e161861ed3a9df401b2a6b7c07ea1475b
dt1993 <60934098+dt1993@users.noreply.github.com>
2020-07-04 23:18:25 -0500

    add hide and show functions in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,17 @@
 var playing = false;
 var score;
+var action;
+var timeremaining
+
 
 //if we click on the start/reset button
 document.getElementById("startreset").onclick = function(){
     //if we are playing
     if(playing == true){
     location.reload(); //reload page
-    }else{//if we are not playing
+
+    //if we are not playing
+    }else{
         //reload page
         playing = true;
         //set score to 0
@@ -15,13 +20,25 @@ document.getElementById("startreset").onclick = function(){
 document.getElementById("scorevalue").innerHTML = score;
 
 //show countdown box
-document.getElementById("timeremaining").style.display =" block";
+        show("timeremaining");
+        timeremaining = 5;
+
+document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+
+//hide game over box
+        hide("gameOver");
 
 //change button to reset
-document.getElementById("startreset").innerHTML = "Reset Game"
+document.getElementById("startreset").innerHTML = "Reset Game";
+
+//start countdown
+startCountDown();
+
+//generate new Q&Q
+generateQA();
+
 
 }
-
 }
         
   
@@ -45,3 +62,44 @@ document.getElementById("startreset").innerHTML = "Reset Game"
             //no
                 //show try again box
 
+ //functions           
+
+ //start counter
+function startCountDown(){
+    action = setInterval(function(){
+        timeremaining -= 1;
+
+document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        if(timeremaining == 0) {//game over
+            stopCountDown();
+            show("gameOver");
+
+document.getElementById('gameOver').innerHTML = "<p>game over!</p><p>your score is " + score + ".</p> ";
+        hide("timeremaining");
+        hide("correct");
+        hide("wrong");
+        playing = false;
+        document.getElementById("startreset").innerHTML = "Start Game";
+        }
+    }, 1000);
+}
+
+//stop counter
+function stopCountDown(){
+    clearInterval(action);
+}
+
+//hide element
+function hide(Id){
+    document.getElementById(Id).style.display = "none";
+}
+
+//show element
+function show(Id){
+    document.getElementById(Id).style.display = "block";
+}
+
+//generateQ&A
+function generateQA(){
+    
+}
