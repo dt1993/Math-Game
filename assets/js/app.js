445a9af76830f4b37a4631c116d2e1ac7e9f5c0a
dt1993 <60934098+dt1993@users.noreply.github.com>
2020-07-04 22:23:12 -0500

    update js to start button and time remaining
    start/restart, reload page, set score, show time remaining

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,47 @@
-js file
+var playing = false;
+var score;
+
+//if we click on the start/reset button
+document.getElementById("startreset").onclick = function(){
+    //if we are playing
+    if(playing == true){
+    location.reload(); //reload page
+    }else{//if we are not playing
+        //reload page
+        playing = true;
+        //set score to 0
+        score = 0;
+
+document.getElementById("scorevalue").innerHTML = score;
+
+//show countdown box
+document.getElementById("timeremaining").style.display =" block";
+
+//change button to reset
+document.getElementById("startreset").innerHTML = "Reset Game"
+
+}
+
+}
+        
+  
+       
+       
+       //reduce time by 1 sec in loops
+             //time left?
+                 //yes -> continue
+                 //no -> gameover
+       
+       //generate new Q&A
+
+
+//if we click on an answer box
+    //if we are playing
+       //correct?
+            //yes
+                //increase score 
+                //show correct box for 1 sec
+                //generate new Q&A
+            //no
+                //show try again box
+
